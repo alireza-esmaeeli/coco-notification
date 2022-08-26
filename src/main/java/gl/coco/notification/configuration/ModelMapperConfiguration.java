f0922def267f649ecc934e4147e8e2d1546ca7e9
Alireza Esmaeeli <a.esmaeeli@rahnema.com>
2022-08-26 23:43:31 +0430

    Configure model mapper

---
+++ src/main/java/gl/coco/notification/configuration/ModelMapperConfiguration.java
@@ -0,0 +1,23 @@
+package gl.coco.notification.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+import static org.modelmapper.convention.MatchingStrategies.STANDARD;
+
+@Configuration
+public class ModelMapperConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(PRIVATE)
+                .setMatchingStrategy(STANDARD);
+        return modelMapper;
+    }
+}
